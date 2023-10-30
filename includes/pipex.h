82a4c4bb633d25d0c0add3b711dd81de5c42336d
osarsari <o.sarsari@gmail.com>
2023-10-30 12:07:25 +0100

    starting

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 11:43:21 by osarsari          #+#    #+#             */
-/*   Updated: 2023/10/30 11:50:05 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:04:58 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,13 @@
 # include <stdio.h>
 # include <fcntl.h>
 # include <sys/wait.h>
+
+typedef struct s_cmd
+{
+	struct s_cmd	*next;
+	char			**redir_in;
+	char			**redir_out;
+	char			**args;
+}					t_cmd;
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,8 +6,20 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 11:36:15 by osarsari          #+#    #+#             */
-/*   Updated: 2023/10/30 11:50:11 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:06:52 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_cmd	**cmds;
+
+	if (argc != 5)
+	{
+		ft_putstr_fd("Error: wrong number of arguments\n", 2);
+		return (1);
+	}
+	cmds = build_cmd(argv, envp);
+}
